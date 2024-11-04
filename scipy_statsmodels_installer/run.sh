123282a4c536919aade661a3e32d7888ef79f5c7
Enrique <enriquemg.75@gmail.com>
2024-11-04 11:29:41 +0100

    Update run.sh

--- scipy_statsmodels_installer/run.sh
+++ scipy_statsmodels_installer/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/with-contenv bashio
-PYTHON_VERSION=$(python3 -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
 
-apk add py3-scipy
-ln -s /usr/lib/python${PYTHON_VERSION}/site-packages/scipy /usr/local/lib/python${PYTHON_VERSION}/site-packages/scipy
+# Este archivo queda vacío en esta configuración, pero puedes utilizarlo
+# para futuras configuraciones adicionales si lo necesitas.
+
+echo "SciPy se ha instalado correctamente mediante pip."
