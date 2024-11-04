0fa36d76c70630fa33152bd66ddfc2242640f24c
Enrique <enriquemg.75@gmail.com>
2024-11-04 11:12:09 +0100

    Update run.sh

--- scipy_statsmodels_installer/run.sh
+++ scipy_statsmodels_installer/run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/with-contenv bashio
 PYTHON_VERSION=$(python3 -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
 
-apk add py3-scipy py3-statsmodels
+apk add py3-scipy
 ln -s /usr/lib/python${PYTHON_VERSION}/site-packages/scipy /usr/local/lib/python${PYTHON_VERSION}/site-packages/scipy
-ln -s /usr/lib/python${PYTHON_VERSION}/site-packages/statsmodels /usr/local/lib/python${PYTHON_VERSION}/site-packages/statsmodels
